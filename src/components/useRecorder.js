3792ff45d3a5bda64929c1ab10f5c6656e31d773
Sıla Mahim <silamahim@Sla-MacBook-Air.local>
2025-01-12 17:41:53 +0300

    sesler eklendi

--- src/components/useRecorder.js
+++ src/components/useRecorder.js
@@ -5,7 +5,7 @@ export default function useRecorder() {
   const [isRecording, setIsRecording] = useState(false);
   const mediaRecorderRef = useRef(null);
   const audioChunks = useRef([]);
-  2;
+
   const startRecording = async () => {
     const stream = await navigator.mediaDevices.getUserMedia({ audio: true });
     mediaRecorderRef.current = new MediaRecorder(stream);
@@ -32,13 +32,12 @@ export default function useRecorder() {
     mediaRecorderRef.current.stop();
     setIsRecording(false);
   };
-  //burası değişecek
+
   const uploadAudio = async (audioBlob) => {
     const formData = new FormData();
     formData.append("audio", audioBlob, "recording.mp3");
 
     try {
-      // Ses dosyasını yükle
       const response = await fetch("http://localhost:5001/upload", {
         method: "POST",
         body: formData,
