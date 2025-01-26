90f358db28c2b5af1c38f1507c4d2029ae677be0
Sıla Mahim <silamahim@Sla-MacBook-Air.home>
2025-01-27 01:09:10 +0300

    sil

--- backend/server.js
+++ backend/server.js
@@ -3,7 +3,7 @@ const multer = require("multer");
 const path = require("path");
 
 const app = express();
-//eklenecek
+
 const { exec } = require("child_process");
 
 const compareAudioFiles = (file1, file2) => {
