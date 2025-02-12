e9e57e1c06ff75ced55961f7ed3af1bfd256d40d
Sıla Mahim <silamahim@Sla-MacBook-Air.local>
2025-02-12 21:24:14 +0300

    ekle

--- backend/server.js
+++ backend/server.js
@@ -34,7 +34,7 @@ compareAudioFiles(file1, file2)
     console.error(err);
   });
 
-// Multer için depolama ayarlanacak
+// Multer için depolama ayarlana
 const storage = multer.diskStorage({
   destination: (req, file, cb) => {
     cb(null, "uploads/"); // Dosyanın kaydedileceği klas
